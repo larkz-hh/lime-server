61feb3861afcf0d970b6e163fe46a0723677ba82
larkz-hh <3632378642@qq.com>
2026-07-05 21:46:30 +0800

    Initial commit

---
+++ src/main/java/com/lzz/lime_server/LimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.lzz.lime_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzz/lime_server/LimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.lime_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
