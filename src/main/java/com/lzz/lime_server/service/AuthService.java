9f32f6c8f8ff2ed751ed999f699a0fa38a74d63d
larkz-hh <3632378642@qq.com>
2026-07-13 02:59:13 +0800

    feat(auth): AuthService 接口添加加 sendCode 方法

--- src/main/java/com/lzz/lime_server/service/AuthService.java
+++ src/main/java/com/lzz/lime_server/service/AuthService.java
@@ -3,13 +3,16 @@ package com.lzz.lime_server.service;
 import com.lzz.lime_server.dto.request.LoginRequest;
 import com.lzz.lime_server.dto.request.RefreshTokenRequest;
 import com.lzz.lime_server.dto.request.RegisterRequest;
+import com.lzz.lime_server.dto.request.SendCodeRequest;
 import com.lzz.lime_server.dto.response.LoginResponse;
 
 /**
  * 认证服务接口
- * 册、登录、登出、Token 刷新
+ * 发验证码、注册、登录、登出、Token 刷新
  */
 public interface AuthService {
+
+    void sendCode(SendCodeRequest request);
 
     void register(RegisterRequest request);
 
