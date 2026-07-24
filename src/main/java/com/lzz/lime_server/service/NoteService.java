a1050495cb64284ce8d5bf9f909e768a8659f525
larkz-hh <3632378642@qq.com>
2026-07-24 23:28:44 +0800

    feat(like): 完成笔记点赞与收藏模块的服务层定义与实现

--- src/main/java/com/lzz/lime_server/service/NoteService.java
+++ src/main/java/com/lzz/lime_server/service/NoteService.java
@@ -2,6 +2,7 @@ package com.lzz.lime_server.service;
 
 import com.lzz.lime_server.dto.request.PublishNoteRequest;
 import com.lzz.lime_server.dto.response.CursorPage;
+import com.lzz.lime_server.dto.response.NoteDetailResponse;
 import com.lzz.lime_server.dto.response.NoteFeedResponse;
 import com.lzz.lime_server.dto.response.NoteResponse;
 
@@ -9,4 +10,12 @@ public interface NoteService {
     NoteResponse publishNote(Long userId, PublishNoteRequest request);
 
     CursorPage<NoteFeedResponse> getFeed(Long cursor, int size);
+
+    void likeNote(Long noteId, Long userId);
+
+    void unlikeNote(Long noteId, Long userId);
+
+    void favoriteNote(Long noteId, Long userId);
+
+    void unfavoriteNote(Long noteId, Long userId);
 }
